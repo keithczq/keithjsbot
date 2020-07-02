f758021d7563ec525a6a443b419163e72db4524e
Keith Chua <keithchua@Keiths-MBP.attlocal.net>
2020-07-02 15:28:18 -0700

    working server on localhost

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Keith JS Bot
-### Soon... I will replace all of Keith's communications
+Soon... I will replace all of Keith's communications
 
-# About
-This project is a Telegram chatbot that is meant to act as the server for incoming messages to the bot. The @name of the bot will be revealed once a working demo is out!
+## About
+This project is a Telegram chatbot that is meant to act as the server for incoming messages to the bot. You can find the bot on Telegram at @keithjsbot or https://t.me/keithjsbot .
+
+## List of Available commands
+* /start - Displays a simple welcome message when user first talks to bot
+* /echo - Usage: /echo (input text without the parentheses)
+* /keithsays - testing testing...
+* /testing - actual lines said by Keith himself!
+
+## Upcoming Features!
+- [x] Upload bot picture
+- [ ] Host on live server!
+- [ ] Do bio of bot
+- [ ] Do description of bot
+- [ ] Add /help command
+- [ ] Add more robust lines for /keithsays command
+- [ ] Create /feedback command for faster feedback channel
+- [ ] Add randomized greetings for hello
+- [ ] Add randomized greetings for goodbye
+- [ ] Create quiz command game for lines said by keith (Command to be /quiz?)
+
+## Contact
+Let me know what you think about this project! I welcome constructive criticisms, feedback, suggestions! Email me at keithczq@gmail.com. I look forward to hearing from you!
--- index.js
+++ index.js
@@ -1,29 +1,88 @@
+///////////////////// Imports
 const TelegramBot = require('node-telegram-bot-api');
+const config = require('./config.json');
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+///////////////////// Global constants
+const token = config.token;
+const bot = new TelegramBot(token, {
+  polling: true
+});
+const botName = "@keithjsbot";
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+///////////////////// Commands handlers
+// Greeting message when user starts initial chat with bot
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome! My name is the Keith JS Bot 1.0 and it is nice to meet you!");
+});
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
-
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+// Says a random line that actual Keith Says
+bot.onText(/\/keithsays/, (msg) => {
+  bot.sendMessage(msg.chat.id, "pew pew pew!");
+});
+
+// just to repeat 1 2 3 to the user
+bot.onText(/\/testing/, (msg) => {
+  bot.sendMessage(msg.chat.id, "1.. 2.. 3..😜");
+  return;
+});
+
+/////////////////////  All other message handlers
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  const firstName = msg.chat.first_name;
+  const lastName = msg.chat.last_name;
+  const msgContents = msg.text.toString().toLowerCase();
+  // // send a message to the chat acknowledging receipt of their message
+  bot.sendChatAction(msg.chat.id, "typing");
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  // Escaping command handlers
+  // TODO: change to a list that checks against existing commands instead!!
+  if (msgContents.indexOf("/") === 0) {
+    return;
+  }
+
+  // Greetings handlers
+  if (msgContents.includes("hi") || msgContents.includes("hello") || msgContents.includes("yo")) {
+    bot.sendMessage(msg.chat.id, `Hello dear ${firstName} ${lastName}\nFor your information, your Chat ID is: ${chatId}`);
+    return;
+  }
+
+  if (msgContents.includes("bye")) {
+    bot.sendMessage(msg.chat.id, `Bye ${firstName}, Hope to see you soon`);
+    bot.sendMessage(msg.chat.id, `You can always chat with me again <b>${botName}</b> `, {
+      parse_mode: "HTML"
+    });
+    bot.sendMessage(msg.chat.id, `Otherwise, feel free to check out my creator's <b><a href="https://keithczq.github.io/">website</a></b>`, {
+      parse_mode: "HTML"
+    });
+    return;
+  }
+
+  // Profanity handlers
+  // FIXME: not working!
+  if (msg.text.includes("shit")) {
+    bot.kickChatMember(msg.chat.id, msg.from.id);
+    return;
+  }
+
+  // Exception handler
+  bot.sendMessage(msg.chat.id, `Sorry, I don't quite understand. I've got nothing...😅`);
+  bot.sendMessage(msg.chat.id, `It is going to take as well to be as smart as Keith but I am getting there!🤓`);
+
+  // bot.sendMessage(nonExistentUserId, 'text').catch((error) => {
+  //   console.log(error.code); // => 'ETELEGRAM'
+  //   console.log(error.response.body); // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
+  // });
+
 });
